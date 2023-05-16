8322259e6f428ea971dec68f5be28dc6c6cf2ca5
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-05-17 00:25:39 +0700

    feat: eslint and prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+/** @satisfies {import('@trivago/prettier-plugin-sort-imports').PrettierConfig} */
+const config = {
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  printWidth: 80,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: [
+    '^discord.js',
+    '^@discordjs',
+    '<THIRD_PARTY_MODULES>',
+    '^[.][.]',
+    '^[.]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
+
+module.exports = config
