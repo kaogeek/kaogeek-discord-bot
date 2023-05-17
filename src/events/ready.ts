9bba287c6b3922b17c4480934ca954a2822829d2
masuru-aomsin <skinmaster133@gmail.com>
2023-05-17 05:52:19 +0000

    added: example command & dockerfile

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,5 +5,13 @@ export default {
   once: true,
   execute: async (client) => {
     console.log(`Helloworld, Online as ${client.user?.tag}.`)
+    const commands_data = [...client.commands.values()].map(
+      (command) => command.data
+    )
+    try {
+      await client.application?.commands.set(commands_data)
+    } catch (error) {
+      console.log(error)
+    }
   },
 } satisfies EventHandlerConfig<'ready'>
