1f69da9c2ab10a2c12fc10e685b98584fb02a454
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-05-19 06:00:01 +0700

    refactor: static import (#75)

---
+++ src/events/index.ts
@@ -0,0 +1,5 @@
+import interactionCreate from './interactionCreate.js'
+import preventEmojiSpam from './preventEmojiSpam.js'
+import ready from './ready.js'
+
+export default [interactionCreate, preventEmojiSpam, ready]
