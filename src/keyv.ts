8e42f7ac7e8c93ad5210732f1343889c660190d3
masuru-aomsin <skinmaster133@gmail.com>
2023-05-18 07:09:40 +0000

    format code

--- src/keyv.ts
+++ src/keyv.ts
@@ -1,5 +1,6 @@
-import Keyv from 'keyv'
 import fs from 'fs'
+import Keyv from 'keyv'
+
 // Check Folder: ./.data/db.sqlite
 if (!fs.existsSync('./.data')) fs.mkdirSync('./.data')
 export const keyv = new Keyv('sqlite://./.data/db.sqlite')
