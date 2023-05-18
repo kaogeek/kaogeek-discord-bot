69b8fa135f6f015e37c802e9be71778333426da0
samithiwat <samithiwat2544@gmail.com>
2023-05-18 21:34:46 +0700

    chore: mock env

--- test/spec/client.spec.ts
+++ test/spec/client.spec.ts
@@ -1,8 +1,18 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 
 import Bot from '../../src/client'
 
+vi.mock('../../src/config.js', () => {
+  const Environment = { BOT_TOKEN: 'MOCK_TOKEN' }
+
+  return { Environment }
+})
+
 describe('Bot', () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
   it('should defined', async () => {
     expect(new Bot()).toBeDefined()
   })
