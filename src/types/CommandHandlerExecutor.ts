d8894c4dcf8c26f8081e3c509e611ea122187e04
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-05-19 02:42:49 +0700

    refactor: update method name

--- src/types/CommandHandlerExecutor.ts
+++ src/types/CommandHandlerExecutor.ts
@@ -1,8 +1,8 @@
-import { CommandInteraction } from 'discord.js'
+import { Awaitable, CommandInteraction } from 'discord.js'
 
 import type Bot from '../client.js'
 
 export type CommandHandlerExecutor = (
   client: Bot,
   interaction: CommandInteraction,
-) => void
+) => Awaitable<void>
