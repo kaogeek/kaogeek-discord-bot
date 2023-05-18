9f2ed392c95f299a9f53b0c51025fcf6b743653f
samithiwat <samithiwat2544@gmail.com>
2023-05-18 22:58:48 +0700

    feat: add confirmation banner

--- src/commands/moderators/deleteAllMessage.ts
+++ src/commands/moderators/deleteAllMessage.ts
@@ -1,6 +1,8 @@
 import {
   ApplicationCommandType,
+  ButtonStyle,
   ChannelType,
+  ComponentType,
   DiscordAPIError,
   PermissionsBitField,
 } from 'discord.js'
@@ -18,7 +20,44 @@ export default {
   execute: async (client, interaction) => {
     if (!interaction.guild || !interaction.isContextMenuCommand()) return
 
-    // TODO: confirmation banner
+    // TODO: Confirmation banner
+    await interaction.followUp({
+      content: 'Are you sure you want to prune all messages from this user?',
+      components: [
+        {
+          type: ComponentType.ActionRow,
+          components: [
+            {
+              type: ComponentType.Button,
+              style: ButtonStyle.Primary,
+              label: 'Yes',
+              customId: 'yes',
+            },
+            {
+              type: ComponentType.Button,
+              style: ButtonStyle.Danger,
+              label: 'No',
+              customId: 'no',
+            },
+          ],
+        },
+      ],
+    })
+
+    // Await button interaction for confirmation
+    const buttonInteraction = await interaction.channel?.awaitMessageComponent({
+      filter: (i) => i.customId === 'yes' || i.customId === 'no',
+      time: 10000, // Adjust timeout as needed
+    })
+
+    if (buttonInteraction?.customId === 'no') {
+      // Reply about the cancel action
+      await interaction.editReply({
+        content: `Prune message was canceled`,
+        components: [],
+      })
+      return
+    }
 
     // Delete message in all channel
     const message = await interaction.channel?.messages.fetch(
@@ -39,12 +78,18 @@ export default {
               `Deleted ${userMessages.size} messages from ${interaction.targetId} in channel ${channel.name} (${channelId}).`,
             )
             // Tell the user that the messages were successfully pruned
-            await interaction.editReply('Successfully prune messages')
+            await interaction.editReply({
+              content: 'Successfully prune messages',
+              components: [],
+            })
           } catch (error) {
             // Reply about the error
-            await interaction.editReply(
-              `Error deleting messages: ${(error as DiscordAPIError).message}`,
-            )
+            await interaction.editReply({
+              content: `Error deleting messages: ${
+                (error as DiscordAPIError).message
+              }`,
+              components: [],
+            })
             console.error('Error deleting messages:', error)
           }
         }
