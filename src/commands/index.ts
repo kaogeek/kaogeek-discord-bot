5a0d611173dd2efc32b1c19b8c94640940bc5ebb
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-05-19 03:24:59 +0700

    refactor: static import

---
+++ src/commands/index.ts
@@ -0,0 +1,6 @@
+import ping from './info/ping.js'
+import deleteAllMessage from './moderators/deleteAllMessage.js'
+import report from './moderators/report.js'
+import user from './moderators/user.js'
+
+export default [ping, deleteAllMessage, report, user]
