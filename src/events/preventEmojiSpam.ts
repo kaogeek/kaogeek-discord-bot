ce58546b3aa3444dd890c49c89663c17f5be6a8c
samithiwat <samithiwat2544@gmail.com>
2023-05-18 20:01:13 +0700

    Prevent Only Emoji Spam in Chat (#36)
    * feat: add issue template
    
    * chore: remove unused statement
    
    * chore: change header message
    
    * chore: ignore sqlite
    
    * feat: implement prevent only emoji feature
    
    * fix: only message error
    
    * chore: add emoji component in regex
    
    * fix: typo

---
+++ src/events/preventEmojiSpam.ts
@@ -0,0 +1,23 @@
+import { Events, Message } from 'discord.js'
+
+import { EventHandlerConfig } from '../types/EventHandlerConfig'
+
+const emojiRegex =
+  /<a?(:\w+:\d+)>|\p{Emoji_Presentation}|\p{Extended_Pictographic}|\p{Emoji_Component}/gu
+
+export default {
+  eventName: Events.MessageCreate,
+  once: false,
+  execute: async (client, message) => {
+    const emoji = message.content.match(emojiRegex)
+
+    // if has only emoji -> delete message
+    if (emoji && emoji.join('').trim() === message.content.trim()) {
+      try {
+        message.delete()
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  },
+} satisfies EventHandlerConfig<Events.MessageCreate>
