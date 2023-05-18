f2d7ba8f317d0062b1e88289f546a414398f0176
samithiwat <samithiwat2544@gmail.com>
2023-05-18 14:56:33 +0700

    feat: implement prevent only emoji feature

---
+++ src/events/preventEmojiSpam.ts
@@ -0,0 +1,22 @@
+import { Events } from 'discord.js'
+
+import { EventHandlerConfig } from '../types/EventHandlerConfig'
+
+const emojiRegex = /<a?:\w+:\d+>/g
+
+export default {
+  eventName: Events.MessageCreate,
+  once: false,
+  execute: async (client, message) => {
+    const emoji = message.content.match(emojiRegex)
+
+    // if has only emoji -> delete message
+    if (emoji.join('') === message.content) {
+      try {
+        message.delete()
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  },
+} satisfies EventHandlerConfig<Events.MessageCreate>
