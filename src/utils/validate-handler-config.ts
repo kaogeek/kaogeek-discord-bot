efb310e1df357b6a42a4bd9ddfa393f9535c171f
Vatunyoo Suwannapisit <ker.los@hotmail.com>
2023-05-19 01:45:14 +0700

    refactor to use with zod

--- src/utils/validate-handler-config.ts
+++ src/utils/validate-handler-config.ts
@@ -1,23 +1,24 @@
+import { z } from 'zod'
+
 import { CommandHandlerConfig } from '../types/CommandHandlerConfig.js'
 import { EventHandlerConfig } from '../types/EventHandlerConfig.js'
 
+const EventHandlerSchema = z.object({
+  eventName: z.string(),
+  once: z.boolean().optional(),
+  execute: z.function(),
+})
+const CommandHandlerSchama = z.object({
+  data: z.unknown(),
+  ephemeral: z.boolean().optional(),
+  execute: z.function(),
+})
 // Validate config to be appropriate value
 export const validateEventHandlerConfig = (
   config: unknown,
 ): config is EventHandlerConfig => {
-  if (typeof config !== 'object' || config === null) {
-    throw new TypeError('Config must be an object')
-  }
-  if (!('eventName' in config) || typeof config.eventName !== 'string')
-    throw new TypeError('Config must have eventName property of type string')
-
-  // once is optional
-  if ('once' in config && typeof config.once !== 'boolean')
-    throw new TypeError('once must be of type boolean if it exists in config')
-
-  if (!('execute' in config) || typeof config.execute !== 'function')
-    throw new TypeError('Config must have execute property of type function')
-
+  // throw error if invalid
+  EventHandlerSchema.parse(config)
   return true
 }
 
@@ -25,20 +26,7 @@ export const validateEventHandlerConfig = (
 export const validateCommandHandlerConfig = (
   config: unknown,
 ): config is CommandHandlerConfig => {
-  if (typeof config !== 'object' || config === null) {
-    throw new TypeError('Config must be an object')
-  }
-
-  if (!('data' in config) || typeof config.data !== 'object') {
-    throw new TypeError('Config must have data property of type object')
-  }
-
-  // ephemeral is optional
-  if ('ephemeral' in config && typeof config.ephemeral !== 'boolean')
-    throw new TypeError('once must be of type boolean if it exists in config')
-
-  if (!('execute' in config) || typeof config.execute !== 'function')
-    throw new TypeError('Config must have execute property of type function')
-
+  // throw error if invalid
+  CommandHandlerSchama.parse(config)
   return true
 }
