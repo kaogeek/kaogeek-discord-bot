7682e243f3ad1979869ee4192f63924fc2e5eb54
samithiwat <samithiwat2544@gmail.com>
2023-05-18 22:41:27 +0700

    fix: crash message not found

--- src/commands/moderators/deleteAllMessage.ts
+++ src/commands/moderators/deleteAllMessage.ts
@@ -2,7 +2,7 @@ import {
   ApplicationCommandType,
   ChannelType,
   DiscordAPIError,
-  GuildMember,
+  InteractionType,
   PermissionsBitField,
 } from 'discord.js'
 
@@ -12,27 +12,22 @@ export default {
   data: {
     name: 'Prune messages',
     type: ApplicationCommandType.Message,
+    defaultMemberPermissions: PermissionsBitField.Flags.ManageChannels,
+    dmPermission: false,
   },
   ephemeral: true,
   execute: async (client, interaction) => {
     if (!interaction.guild || !interaction.isContextMenuCommand()) return
 
-    // Get the member who initiated the interaction
-    const member = interaction.member as GuildMember
-
-    // Check user permission
-    if (!member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
-      console.warn(`[WARN] ${member.displayName} try to delete all message`)
-      await interaction.editReply(
-        'Invalid permission, this command available for ONLY Moderator',
-      )
-      return
-    }
+    // TODO: confirmation banner
 
     // Delete message in all channel
+    const message = await interaction.channel?.messages.fetch(
+      interaction.targetId,
+    )
+
     for (const [channelId, channel] of client.channels.cache) {
       if (channel.type === ChannelType.GuildText) {
-        const message = await channel.messages.fetch(interaction.targetId)
         const messages = await channel.messages.fetch()
         const userMessages = messages.filter(
           (msg) => msg.author.id === message?.author.id,
@@ -42,7 +37,7 @@ export default {
           try {
             await channel.bulkDelete(userMessages)
             console.info(
-              `Deleted ${userMessages.size} messages from ${interaction.targetId} in channel ${channel.name}.`,
+              `Deleted ${userMessages.size} messages from ${interaction.targetId} in channel ${channel.name} (${channelId}).`,
             )
             // Tell the user that the messages were successfully pruned
             await interaction.editReply('Successfully prune messages')
