19ee1f808a357faaa1816adb10354b00e554d577
samithiwat <samithiwat2544@gmail.com>
2023-05-19 01:04:05 +0700

    refactor: use isOnlyEmoji function

--- src/events/preventEmojiSpam.ts
+++ src/events/preventEmojiSpam.ts
@@ -1,6 +1,7 @@
 import { Events } from 'discord.js'
 
-import { EventHandlerConfig } from '../types/EventHandlerConfig.js'
+import { EventHandlerConfig } from '../types/EventHandlerConfig'
+import isOnlyEmoji from '../utils/isOnlyEmoji'
 
 const emojiRegex =
   /(<a?(:\w+:\d+)>|\p{Emoji_Presentation}|\p{Extended_Pictographic}|\p{Emoji_Component})\s*/gu
@@ -9,10 +10,8 @@ export default {
   eventName: Events.MessageCreate,
   once: false,
   execute: async (client, message) => {
-    const emoji = message.content.match(emojiRegex)
-
     // if has only emoji -> delete message
-    if (emoji && emoji.join('').trim() === message.content.trim()) {
+    if (isOnlyEmoji(message.content)) {
       try {
         await message.delete()
       } catch (err) {
