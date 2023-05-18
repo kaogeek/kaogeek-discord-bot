ab7929c5a85c2825f9f63aac9067fcc327f93fbc
masuru-aomsin <skinmaster133@gmail.com>
2023-05-18 07:06:53 +0000

    added: check folder: ./.data/ Datastore files is exists

--- src/keyv.ts
+++ src/keyv.ts
@@ -1,3 +1,5 @@
 import Keyv from 'keyv'
-
+import fs from 'fs'
+// Check Folder: ./.data/db.sqlite
+if (!fs.existsSync('./.data')) fs.mkdirSync('./.data')
 export const keyv = new Keyv('sqlite://./.data/db.sqlite')
