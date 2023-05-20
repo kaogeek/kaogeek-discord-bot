604a15a029de6102e9cc9ba58fedf8d7eaa9b882
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-05-20 19:40:11 +0700

    (Error) github action check fixed

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,12 @@
 import { CommandHandlerConfig } from '../types/CommandHandlerConfig.js'
 
-import ping from './info/ping.js';
-import kaokaiToday from './users/kaokaiToday.js';
+import ping from './info/ping.js'
 import activeThreads from './moderators/activeThreads.js'
 import deleteAllMessage from './moderators/deleteAllMessage.js'
 import inspectProfile from './moderators/inspectProfile.js'
 import report from './moderators/report.js'
 import user from './moderators/user.js'
+import kaokaiToday from './users/kaokaiToday.js'
 
 export default [
   activeThreads,
--- src/commands/users/kaokaiToday.ts
+++ src/commands/users/kaokaiToday.ts
@@ -1,13 +1,14 @@
-import cheerio from 'cheerio';
+import { CommandInteraction } from 'discord.js'
 
-import { CommandHandlerConfig } from '../../types/CommandHandlerConfig.js';
-import { CommandInteraction } from 'discord.js';
+import cheerio from 'cheerio'
+
+import { CommandHandlerConfig } from '../../types/CommandHandlerConfig.js'
 
 // Fetch HTML Func
 async function fetchHTML(url: string): Promise<string> {
   try {
-    const response = await fetch(url);
-    return await response.text();
+    const response = await fetch(url)
+    return await response.text()
   } catch (error) {
     console.error('Error fetching HTML:', error)
     throw error
@@ -38,10 +39,10 @@ export default {
     ],
   },
   execute: async (client, interaction: any) => {
-    const option = await interaction.options.getSubcommand(); // รับค่า sub-command ที่ถูกเรียกใช้
+    const option = await interaction.options.getSubcommand() // รับค่า sub-command ที่ถูกเรียกใช้
     if (option === 'latest') {
-      const news_websiteURL = 'https://www.moveforwardparty.org/news/';
-      const html = await fetchHTML(news_websiteURL);
+      const news_websiteURL = 'https://www.moveforwardparty.org/news/'
+      const html = await fetchHTML(news_websiteURL)
 
       const $ = cheerio.load(html)
 
@@ -52,22 +53,22 @@ export default {
       let description = ''
 
       posts.each((index, element) => {
-        const post: any = $(element);
-        if (!post || post === undefined) return;
+        const post: any = $(element)
+        if (!post || post === undefined) return
 
-        const titleElement = post.find('div.info > header > h2 > a');
-        const title = titleElement.text();
+        const titleElement = post.find('div.info > header > h2 > a')
+        const title = titleElement.text()
 
-        const dateElement = post.find('a > div > small');
-        const date = dateElement.text();
+        const dateElement = post.find('a > div > small')
+        const date = dateElement.text()
 
-        const postID = post.attr('id').replace('post-', ''); // ดึง ID ของโพสต์
+        const postID = post.attr('id').replace('post-', '') // ดึง ID ของโพสต์
 
-        const postLink = `https://www.moveforwardparty.org/news/${postID}`; // สร้างลิงก์ของโพสต์
-        postLinks.push(postLink); // เพิ่มลิงก์ในรายการ
+        const postLink = `https://www.moveforwardparty.org/news/${postID}` // สร้างลิงก์ของโพสต์
+        postLinks.push(postLink) // เพิ่มลิงก์ในรายการ
 
         description += `[${title}](${postLink})\nDate: ${date}\n---------------------\n`
-      });
+      })
       await interaction.editReply({
         embeds: [
           {
@@ -79,10 +80,10 @@ export default {
             color: 0xff7f00,
           },
         ],
-      });
+      })
     } else if (option === 'highlight') {
-      const news_websiteURL = 'https://www.moveforwardparty.org/';
-      const html = await fetchHTML(news_websiteURL);
+      const news_websiteURL = 'https://www.moveforwardparty.org/'
+      const html = await fetchHTML(news_websiteURL)
 
       const $ = cheerio.load(html)
 
@@ -91,22 +92,21 @@ export default {
       const postLinks = []
       let description_highlight = ''
 
-      const postsToDisplay = posts.slice(0, 7); // Get only the first 7 posts
+      const postsToDisplay = posts.slice(0, 7) // Get only the first 7 posts
 
       postsToDisplay.each((index, element) => {
+        const post: any = $(element)
+        if (!post || post === undefined) return
 
-        const post: any = $(element);
-        if (!post || post === undefined) return;
+        const titleElement = post.find('div.info > header > h2 > a')
+        const title = titleElement.text()
 
-        const titleElement = post.find('div.info > header > h2 > a');
-        const title = titleElement.text();
-
-        const postID = post.attr('id').replace('post-', '');
-        const postLink = `https://www.moveforwardparty.org/news/${postID}`;
-        postLinks.push(postLink);
+        const postID = post.attr('id').replace('post-', '')
+        const postLink = `https://www.moveforwardparty.org/news/${postID}`
+        postLinks.push(postLink)
 
         description_highlight += `[${title}](${postLink})\n---------------------\n`
-      });
+      })
       await interaction.editReply({
         embeds: [
           {
