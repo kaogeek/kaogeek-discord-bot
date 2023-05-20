a1fa60e07c2676bfcea6d81f86a7780c42e04d18
Thai Pangsakulyanont <dtinth@spacet.me>
2023-05-20 06:57:26 +0000

    ‍

---
+++ src/events/guildMemberUpdate.ts
@@ -0,0 +1,33 @@
+import { Events } from 'discord.js'
+
+import { EventHandlerConfig } from '../types/EventHandlerConfig.js'
+
+export default {
+  eventName: Events.GuildMemberUpdate,
+  once: false,
+  execute: async (_client, prev, next) => {
+    if (prev.nickname !== next.nickname) {
+      console.info(
+        `[guildMemberUpdate] "${next.user.tag}" [${next.id}] changed nickname from "${prev.nickname}" to "${next.nickname}" in "${next.guild.name}" [${next.guild.id}]`,
+      )
+    }
+    const prevRoles = new Set(prev.roles.cache.map((role) => role.id))
+    const nextRoles = new Set(next.roles.cache.map((role) => role.id))
+    const addedRoles = [...nextRoles].filter((role) => !prevRoles.has(role))
+    const removedRoles = [...prevRoles].filter((role) => !nextRoles.has(role))
+    for (const roleId of addedRoles) {
+      const role = next.guild.roles.cache.get(roleId)
+      if (!role) continue
+      console.info(
+        `[guildMemberUpdate] "${next.user.tag}" [${next.id}] was given the role "${role.name}" [${role.id}] in "${next.guild.name}" [${next.guild.id}]`,
+      )
+    }
+    for (const roleId of removedRoles) {
+      const role = prev.guild.roles.cache.get(roleId)
+      if (!role) continue
+      console.info(
+        `[guildMemberUpdate] "${next.user.tag}" [${next.id}] was removed from the role "${role.name}" [${role.id}] in "${next.guild.name}" [${next.guild.id}]`,
+      )
+    }
+  },
+} satisfies EventHandlerConfig<Events.GuildMemberUpdate>
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,5 +1,15 @@
+import guildMemberAdd from './guildMemberAdd.js'
+import guildMemberRemove from './guildMemberRemove.js'
+import guildMemberUpdate from './guildMemberUpdate.js'
 import interactionCreate from './interactionCreate.js'
 import preventEmojiSpam from './preventEmojiSpam.js'
 import ready from './ready.js'
 
-export default [interactionCreate, preventEmojiSpam, ready]
+export default [
+  guildMemberAdd,
+  guildMemberRemove,
+  guildMemberUpdate,
+  interactionCreate,
+  preventEmojiSpam,
+  ready,
+]
