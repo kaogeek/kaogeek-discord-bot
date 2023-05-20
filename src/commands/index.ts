3887e494dde06204c9eff83dde1a65fbe81a1ab9
Thai Pangsakulyanont <dtinth@spacet.me>
2023-05-20 15:14:26 +0700

    ‍profile inspector (#87)
    * ‍
    
    * ‍
    
    * ‍
    
    * ‍
    
    * ‍
    
    * ‍
    
    * ‍
    
    * ‍
    
    * ‍
    
    * ‍

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,15 @@
+import { CommandHandlerConfig } from '../types/CommandHandlerConfig.js'
+
 import ping from './info/ping.js'
 import deleteAllMessage from './moderators/deleteAllMessage.js'
+import inspectProfile from './moderators/inspectProfile.js'
 import report from './moderators/report.js'
 import user from './moderators/user.js'
 
-export default [ping, deleteAllMessage, report, user]
+export default [
+  ping,
+  deleteAllMessage,
+  ...inspectProfile,
+  report,
+  user,
+] satisfies CommandHandlerConfig[]
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,5 +1,11 @@
+import { AnyEventHandlerConfig } from '../types/EventHandlerConfig.js'
+
 import interactionCreate from './interactionCreate.js'
 import preventEmojiSpam from './preventEmojiSpam.js'
 import ready from './ready.js'
 
-export default [interactionCreate, preventEmojiSpam, ready]
+export default [
+  interactionCreate,
+  preventEmojiSpam,
+  ready,
+] satisfies AnyEventHandlerConfig[]
