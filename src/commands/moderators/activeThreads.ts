97f6a6f2e3165f068da45956d6db932885da4849
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-20 16:12:40 +0700

    add unimplemented actions

--- src/commands/moderators/activeThreads.ts
+++ src/commands/moderators/activeThreads.ts
@@ -1,12 +1,15 @@
 import {
   APIThreadChannel,
   ApplicationCommandType,
+  ComponentType,
+  MessageActionRowComponentData,
   PermissionsBitField,
   RESTGetAPIGuildThreadsResult,
   Routes,
 } from 'discord.js'
 
 import { CommandHandlerConfig } from '../../types/CommandHandlerConfig.js'
+import { ActionSet } from '../../utils/ActionSet.js'
 
 export default {
   data: {
@@ -24,8 +27,59 @@ export default {
     const threads = (data.threads as APIThreadChannel[])
       .slice()
       .sort((a, b) => +(a.last_message_id || 0) - +(b.last_message_id || 0))
+    const actionSet = new ActionSet()
+    const components: MessageActionRowComponentData[] = [
+      {
+        type: ComponentType.StringSelect,
+        customId: actionSet.register('select', async (selectInteraction) => {
+          if (!selectInteraction.isStringSelectMenu()) return
+          const customId = selectInteraction.values[0]
+          const resolved = actionSet.resolve({ customId })
+          if (!resolved) return
+          await resolved.handler(selectInteraction)
+        }),
+        placeholder: 'Select an action',
+        options: [
+          {
+            label: 'Generate report (unimplemented)',
+            value: actionSet.register(
+              'generate-report',
+              async (selectInteraction) => {
+                await selectInteraction.reply({
+                  content: 'Unimplemented!',
+                  ephemeral: true,
+                })
+              },
+            ),
+          },
+          {
+            label: 'Prune old threads (unimplemented)',
+            value: actionSet.register('prune', async (selectInteraction) => {
+              await selectInteraction.reply({
+                content: 'Unimplemented!',
+                ephemeral: true,
+              })
+            }),
+          },
+        ],
+      },
+    ]
     await interaction.editReply({
       content: `There are currently **${threads.length}** active threads in this server.`,
+      components: [
+        {
+          type: ComponentType.ActionRow,
+          components,
+        },
+      ],
     })
+    const action = await actionSet.awaitInChannel(
+      interaction.channel,
+      60e3,
+      interaction.user,
+    )
+    await interaction.editReply({ components: [] }).catch(console.error)
+    if (!action) return
+    await action.registeredAction.handler(action.interaction)
   },
 } satisfies CommandHandlerConfig
