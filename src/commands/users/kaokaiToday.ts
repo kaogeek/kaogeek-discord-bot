8aa44f2f32d9b5452ce3cb386ee4ce484038998d
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-05-20 15:47:36 +0700

    (Command) kaokaiToday change to using fetch

--- src/commands/users/kaokaiToday.ts
+++ src/commands/users/kaokaiToday.ts
@@ -1,14 +1,13 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
+import cheerio from 'cheerio';
 
-import { CommandHandlerConfig } from '../../types/CommandHandlerConfig.js'
+import { CommandHandlerConfig } from '../../types/CommandHandlerConfig.js';
 import { CommandInteraction } from 'discord.js';
 
-//Fetch HTML Func
+// Fetch HTML Func
 async function fetchHTML(url: string): Promise<string> {
   try {
-    const response = await axios.get(url)
-    return response.data
+    const response = await fetch(url);
+    return await response.text();
   } catch (error) {
     console.error('Error fetching HTML:', error)
     throw error
@@ -21,7 +20,7 @@ export default {
     description: 'News from Move Forward Party',
     options: [
       {
-        name: 'latest', 
+        name: 'latest',
         description: 'Latest and up to date news from Move Forward Party',
         descriptionLocalizations: {
           th: 'ข่าวสารก้าวไกลทั้งหมดอัพเดตล่าสุด',
@@ -39,10 +38,10 @@ export default {
     ],
   },
   execute: async (client, interaction: any) => {
-    const option = await interaction.options.getSubcommand() // รับค่า sub-command ที่ถูกเรียกใช้
+    const option = await interaction.options.getSubcommand(); // รับค่า sub-command ที่ถูกเรียกใช้
     if (option === 'latest') {
-      const news_websiteURL = 'https://www.moveforwardparty.org/news/'
-      const html = await fetchHTML(news_websiteURL)
+      const news_websiteURL = 'https://www.moveforwardparty.org/news/';
+      const html = await fetchHTML(news_websiteURL);
 
       const $ = cheerio.load(html)
 
@@ -53,22 +52,22 @@ export default {
       let description = ''
 
       posts.each((index, element) => {
-        const post: any = $(element)
-        if (!post || post === undefined) return
+        const post: any = $(element);
+        if (!post || post === undefined) return;
 
-        const titleElement = post.find('div.info > header > h2 > a')
-        const title = titleElement.text()
-        
-        const dateElement = post.find('a > div > small')
-        const date = dateElement.text()
+        const titleElement = post.find('div.info > header > h2 > a');
+        const title = titleElement.text();
 
-        const postID = post.attr('id').replace('post-', '') // ดึง ID ของโพสต์
+        const dateElement = post.find('a > div > small');
+        const date = dateElement.text();
 
-        const postLink = `https://www.moveforwardparty.org/news/${postID}` // สร้างลิงก์ของโพสต์
-        postLinks.push(postLink) // เพิ่มลิงก์ในรายการ
+        const postID = post.attr('id').replace('post-', ''); // ดึง ID ของโพสต์
+
+        const postLink = `https://www.moveforwardparty.org/news/${postID}`; // สร้างลิงก์ของโพสต์
+        postLinks.push(postLink); // เพิ่มลิงก์ในรายการ
 
         description += `[${title}](${postLink})\nDate: ${date}\n---------------------\n`
-      })
+      });
       await interaction.editReply({
         embeds: [
           {
@@ -76,10 +75,10 @@ export default {
             color: 0xff7f00,
           },
         ],
-      })
+      });
     } else if (option === 'highlight') {
-      const news_websiteURL = 'https://www.moveforwardparty.org/'
-      const html = await fetchHTML(news_websiteURL)
+      const news_websiteURL = 'https://www.moveforwardparty.org/';
+      const html = await fetchHTML(news_websiteURL);
 
       const $ = cheerio.load(html)
 
@@ -92,18 +91,18 @@ export default {
 
       posts.each((index, element) => {
         if (counter >= 7) {
-          return false // Break the loop if the counter exceeds 7
+          return false; // ดึงแค่ 7 โพสต์ใน slider
         }
 
-        const post: any = $(element)
-        if (!post || post === undefined) return
+        const post: any = $(element);
+        if (!post || post === undefined) return;
 
-        const titleElement = post.find('div.info > header > h2 > a')
-        const title = titleElement.text()
+        const titleElement = post.find('div.info > header > h2 > a');
+        const title = titleElement.text();
 
-        const postID = post.attr('id').replace('post-', '')
-        const postLink = `https://www.moveforwardparty.org/news/${postID}`
-        postLinks.push(postLink)
+        const postID = post.attr('id').replace('post-', '');
+        const postLink = `https://www.moveforwardparty.org/news/${postID}`;
+        postLinks.push(postLink);
 
         description_highlight += `[${title}](${postLink})\n---------------------\n`
 
