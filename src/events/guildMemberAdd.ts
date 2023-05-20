312a1942e0d25bde6dd4026c1cffcbe192593c87
samithiwat <samithiwat2544@gmail.com>
2023-05-20 19:39:08 +0700

    refactor: merge to existing event

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,11 +1,33 @@
 import { Events } from 'discord.js'
 
+import { Environment } from '../config.js'
+import { prisma } from '../prisma.js'
 import { defineEventHandler } from '../types/defineEventHandler.js'
 
 export default defineEventHandler({
   eventName: Events.GuildMemberAdd,
   once: false,
-  execute: async (botContext, member) => {
+  execute: async (_client, member) => {
+    try {
+      const userRole = await prisma.userRole.findUnique({
+        where: {
+          userId_roleId: {
+            userId: member.user.id,
+            roleId: Environment.FLAG_ROLE_ID,
+          },
+        },
+      })
+
+      if (userRole !== null && userRole.roleId === Environment.FLAG_ROLE_ID) {
+        await member.roles.add(userRole.roleId)
+        console.info(`flag ${member.user.tag} (rejoin server)`)
+      }
+    } catch (err) {
+      console.error(
+        `error while flag ${member.user.tag}: ${(err as Error).message}`,
+      )
+    }
+
     console.info(
       `[guildMemberAdd] "${member.user.tag}" [${member.id}] joined "${member.guild.name}" [${member.guild.id}]`,
     )
--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,6 +1,10 @@
 import { Events } from 'discord.js'
 
+import { Environment } from '../config.js'
+import { prisma } from '../prisma.js'
 import { defineEventHandler } from '../types/defineEventHandler.js'
+
+const trackingRole = new Set([Environment.FLAG_ROLE_ID])
 
 export default defineEventHandler({
   eventName: Events.GuildMemberUpdate,
@@ -21,20 +25,50 @@ export default defineEventHandler({
         )
       }
     }
+
     const prevRoles = new Set(prev.roles.cache.map((role) => role.id))
     const nextRoles = new Set(next.roles.cache.map((role) => role.id))
     const addedRoles = [...nextRoles].filter((role) => !prevRoles.has(role))
     const removedRoles = [...prevRoles].filter((role) => !nextRoles.has(role))
+
     for (const roleId of addedRoles) {
       const role = next.guild.roles.cache.get(roleId)
       if (!role) continue
+
+      if (trackingRole.has(roleId) && !prevRoles.has(roleId)) {
+        try {
+          await prisma.userRole.create({
+            data: {
+              roleId,
+              userId: next.user.id,
+            },
+          })
+          console.info(`added ${next.user.tag} to ${role.name}`)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+
       console.info(
         `[guildMemberUpdate] "${next.user.tag}" [${next.id}] was given the role "${role.name}" [${role.id}] in "${next.guild.name}" [${next.guild.id}]`,
       )
     }
+
     for (const roleId of removedRoles) {
       const role = prev.guild.roles.cache.get(roleId)
       if (!role) continue
+
+      if (trackingRole.has(roleId) && !nextRoles.has(roleId)) {
+        try {
+          await prisma.userRole.delete({
+            where: { userId_roleId: { roleId, userId: next.user.id } },
+          })
+          console.info(`removed ${next.user.tag} from ${role.name}`)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+
       console.info(
         `[guildMemberUpdate] "${next.user.tag}" [${next.id}] was removed from the role "${role.name}" [${role.id}] in "${next.guild.name}" [${next.guild.id}]`,
       )
