b9b9bfa521b06093620ea87832d3381dc35feeda
samithiwat <samithiwat2544@gmail.com>
2023-05-21 15:49:25 +0700

    chore: add docs comment

--- src/utils/isOnlyEmoji.ts
+++ src/utils/isOnlyEmoji.ts
@@ -1,6 +1,11 @@
 const emojiRegex =
   /(<a?(:\w+:\d+)>|\p{Emoji_Presentation}|\p{Extended_Pictographic}|\p{Emoji_Component})\s*/gu
 
+/**
+ * Check is only emoji message
+ * @param msg - the input message
+ * @returns true if message has only emoji other return false
+ */
 export default (msg: string): boolean => {
   const emoji = msg.match(emojiRegex)
 
