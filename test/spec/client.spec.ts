303fd71e6aff9ed2285ddab8830310626641bd8b
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-21 20:36:31 +0700

    ‌

--- test/spec/client.spec.ts
+++ test/spec/client.spec.ts
@@ -1,6 +1,6 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
-import Bot from '../../src/client'
+import Bot from '../../src/client.js'
 
 vi.mock('../../src/config.js', () => {
   const Environment = { BOT_TOKEN: 'MOCK_TOKEN' }
