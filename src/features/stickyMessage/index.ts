35ec42231f756a13a6a4bd1cce7a07703a4da29b
samithiwat <samithiwat2544@gmail.com>
2023-05-21 20:19:43 +0700

    refactor: rename initStickyMessageCache to initStickyMessage

--- src/features/stickyMessage/index.ts
+++ src/features/stickyMessage/index.ts
@@ -1,14 +1,17 @@
 import { prisma } from '../../prisma.js'
 import { saveCache } from '../../utils/cache.js'
 
+import { resetCooldown } from './messageCooldown.js'
+
 /**
  *
  *  Init sticky message memory cache
  *
  */
-export async function initStickyMessageCache() {
+export async function initStickyMessage() {
   const messages = await prisma.stickyMessage.findMany()
   for (const message of messages) {
     saveCache(message.channelId, message)
+    resetCooldown(message.channelId)
   }
 }
