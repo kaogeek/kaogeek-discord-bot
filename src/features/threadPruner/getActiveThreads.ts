a72bb98ae14f18c5a9cc03fcdba5c58e6a7210cc
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-22 00:02:21 +0700

    ‌

--- src/features/threadPruner/getActiveThreads.ts
+++ src/features/threadPruner/getActiveThreads.ts
@@ -12,5 +12,8 @@ export async function getActiveThreads(
   const result = (await client.rest.get(
     Routes.guildActiveThreads(guild.id),
   )) as RESTGetAPIGuildThreadsResult
+
+  // Discord.js says that `threads` is an `APIChannel`, but it is actually an
+  // `APIThreadChannel`, so a type assertion is needed.
   return result.threads as APIThreadChannel[]
 }
