8c5627e398b2c0ee1220340a72418411e2cb721b
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-21 20:55:51 +0700

    ‌

--- src/commands/runtimeConfig/showConfig.ts
+++ src/commands/runtimeConfig/showConfig.ts
@@ -1,22 +1,18 @@
-import { PermissionsBitField } from 'discord.js'
-
 import { defineCommandHandler } from '../../types/defineCommandHandler.js'
 
 export default defineCommandHandler({
   data: {
-    name: 'reload-config',
-    description: 'Reload configuration',
-    defaultMemberPermissions: PermissionsBitField.Flags.ManageGuild,
+    name: 'show-config',
+    description: 'Show current configuration',
   },
   ephemeral: true,
   execute: async ({ runtimeConfiguration }, interaction) => {
-    const data = await runtimeConfiguration.reload()
     await interaction.editReply({
       content: [
-        'Configuration has been reloaded.',
+        'Current configuration:',
         '',
         '```json',
-        JSON.stringify(data, null, 2),
+        JSON.stringify(runtimeConfiguration.data, null, 2),
         '```',
       ].join('\n'),
     })
