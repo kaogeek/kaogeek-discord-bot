11cbf7b8abc5b775ccd33f94e7f45b468f62c15e
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-26 01:18:59 +0700

    fix

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,6 +2,5 @@ import { Plugin } from '@/types/Plugin'
 
 import report from './moderators/report'
 import slowmode from './moderators/slowmode'
-import user from './moderators/user'
 
-export default [report, user, slowmode] satisfies Plugin[]
+export default [report, slowmode] satisfies Plugin[]
