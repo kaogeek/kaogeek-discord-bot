e2a24e699a1b79edc1efc33d560783a4f98f6b94
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-25 22:09:35 +0700

    ugh. this is why unit tests makes refactoring harder

--- src/types/defineEventHandler.ts
+++ src/types/defineEventHandler.ts
@@ -6,11 +6,21 @@ import { definePlugin } from './definePlugin'
 
 export function defineEventHandler<K extends keyof ClientEvents>(
   config: EventHandlerConfig<K> & { displayName?: string },
-): Plugin {
-  return definePlugin({
-    name: `LegacyEventHandlerPlugin[${config.displayName ?? config.eventName}]`,
-    setup: (plugin) => {
-      plugin.addEventHandler(config)
-    },
-  })
+): Plugin & { execute: EventHandlerConfig<K>['execute'] } {
+  return Object.assign(
+    definePlugin({
+      name: `LegacyEventHandlerPlugin[${
+        config.displayName ?? config.eventName
+      }]`,
+      setup: (plugin) => {
+        plugin.addEventHandler(config)
+      },
+    }),
+
+    // XXX: The `execute` function is not part of the plugin definition,
+    // but it is exposed here because some unit tests need to call it.
+    // Unit tests should not rely on implementation details like this,
+    // because they are hard to refactor.
+    { execute: config.execute },
+  )
 }
