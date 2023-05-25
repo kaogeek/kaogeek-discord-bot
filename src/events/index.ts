1df3b58ed34d80d2190dfa46d82bbe515e327957
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-25 23:52:56 +0700

    move ready/interactionCreate into core

--- src/events/index.ts
+++ src/events/index.ts
@@ -3,13 +3,9 @@ import { Plugin } from '@/types/Plugin'
 import guildMemberAdd from './guildMemberAdd'
 import guildMemberRemove from './guildMemberRemove'
 import guildMemberUpdate from './guildMemberUpdate'
-import interactionCreate from './interactionCreate'
-import ready from './ready'
 
 export default [
   guildMemberAdd,
   guildMemberRemove,
   guildMemberUpdate,
-  interactionCreate,
-  ready,
 ] satisfies Plugin[]
