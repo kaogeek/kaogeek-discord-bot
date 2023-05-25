0b168b79146e1b251700825fbcd237636e3eddf5
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-26 01:26:56 +0700

    ‌

--- src/features/ping/pingCommand.ts
+++ src/features/ping/pingCommand.ts
@@ -9,10 +9,7 @@ export const pingCommand: CommandHandlerConfig = {
   execute: async ({ client }, interaction) => {
     await interaction.editReply({
       embeds: [
-        {
-          description: `ความหน่วง! ${client.ws.ping}ms`,
-          color: 65280,
-        },
+        { description: `ความหน่วง! ${client.ws.ping}ms`, color: 0x00_ff_00 },
       ],
     })
   },
