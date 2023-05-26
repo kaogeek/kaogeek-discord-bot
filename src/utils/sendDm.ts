f9072487e50f841b6518ab63124fd7b269d2325c
J Phaiboon <phaiboon760@gmail.com>
2023-05-26 13:53:05 +0700

    fix

--- src/utils/sendDm.ts
+++ src/utils/sendDm.ts
@@ -10,34 +10,16 @@ export async function sendDm(
   message: Message,
   payload: MessageCreateOptions,
 ): Promise<void> {
-  // Check if a DM channel with the user does not exist
-  if (!message.author.dmChannel) {
-    try {
-      // Try to create a DM channel with the user
+  try {
+    if (!message.author.dmChannel) {
       await message.author.createDM()
-    } catch (error) {
-      // Handle the case where the user has disabled DMs
-      if (error instanceof DiscordAPIError && error.code === 50_007) {
-        console.warn('user not enable permission to send private message')
-      } else {
-        console.error(error)
-        return
-      }
     }
-
-    // At this point, a DM channel with the user either already existed, or has just been created
-    try {
-      // Try to send the message
-      await message.author.send(payload)
-    } catch (error) {
-      // Handle the case when users change their minds to disable DMs
-      if (error instanceof DiscordAPIError && error.code === 50_007) {
-        console.warn('user not enable permission to send private message')
-      } else {
-        // Log any other errors
-        console.error(error)
-      }
+    await message.author.send(payload)
+  } catch (error) {
+    if (error instanceof DiscordAPIError && error.code === 50_007) {
+      console.warn('user not enable permission to send private message')
+    } else {
+      console.error(error)
     }
-    return
   }
 }
