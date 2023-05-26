51158af49a7685596da93e6c402113ecd8ed24c7
3raphat <treeraphat234@gmail.com>
2023-05-27 01:22:13 +0700

    feat: add temp-role command

---
+++ src/features/tempRole/expireCheck.ts
@@ -0,0 +1,50 @@
+import { Guild, GuildMember, Role } from 'discord.js'
+
+import { Environment } from '@/config'
+import { prisma } from '@/prisma'
+import { BotContext } from '@/types/BotContext'
+
+const timePeriod = 1000
+
+export async function expireCheck(botContext: BotContext) {
+  setInterval(async () => {
+    // Check for expired temporary roles
+    const expiredTemporaryRoles = await prisma.tempRole.findMany({
+      where: {
+        expiresAt: {
+          lte: new Date(),
+        },
+      },
+      select: {
+        guildId: true,
+        userId: true,
+        roleId: true,
+      },
+    })
+    // Remove expired temporary roles from users
+    for (const expiredTemporaryRole of expiredTemporaryRoles) {
+      if (expiredTemporaryRole.guildId !== Environment.GUILD_ID) return
+      const { client } = botContext
+
+      // Get guild, member, and role
+      const guild = client.guilds.cache.get(
+        expiredTemporaryRole.guildId,
+      ) as Guild
+      const member = (await guild.members.fetch(
+        expiredTemporaryRole.userId,
+      )) as GuildMember
+      const role = guild.roles.cache.get(expiredTemporaryRole.roleId) as Role
+
+      // Remove role from member
+      member.roles.remove(role)
+    }
+    // Remove expired temporary roles from database
+    await prisma.tempRole.deleteMany({
+      where: {
+        expiresAt: {
+          lte: new Date(),
+        },
+      },
+    })
+  }, timePeriod)
+}
---
+++ src/features/tempRole/temporaryRoleCommand.ts
@@ -0,0 +1,89 @@
+import {
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+  GuildMember,
+  PermissionsBitField,
+  Role,
+  time,
+} from 'discord.js'
+
+import parse from 'parse-duration'
+
+import { prisma } from '@/prisma'
+import { defineCommand } from '@/types/defineCommand'
+
+export const temporaryRoleCommand = defineCommand({
+  data: {
+    name: 'temp-role',
+    description: 'Set a temporary role for a user',
+    defaultMemberPermissions: PermissionsBitField.Flags.ManageRoles,
+    type: ApplicationCommandType.ChatInput,
+    options: [
+      {
+        name: 'user',
+        description: 'User to set the temporary role for',
+        type: ApplicationCommandOptionType.User,
+        required: true,
+      },
+      {
+        name: 'role',
+        description: 'Role to set',
+        type: ApplicationCommandOptionType.Role,
+        required: true,
+      },
+      {
+        name: 'duration',
+        description: 'Duration of the temporary role e.g. 1d',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+    ],
+  },
+  ephemeral: true,
+  execute: async (botContext, interaction) => {
+    if (!interaction.guild || !interaction.isChatInputCommand()) return
+
+    // Get user from the interaction
+    const user = interaction.options.getMember('user') as GuildMember
+
+    // Get role from the interaction
+    const role = interaction.options.getRole('role') as Role
+
+    // Get duration from the interaction
+    const duration = interaction.options.getString('duration') as string
+    if (parse(duration) === null) {
+      await interaction.editReply({
+        content: `Invalid duration: ${duration}`,
+      })
+      return
+    }
+
+    // Check if the user already has the role
+    if (user.roles.cache.has(role.id)) {
+      await interaction.editReply({
+        content: `User: ${user} already has role: ${role}`,
+      })
+      return
+    }
+
+    const expriesAt = new Date(Date.now() + parse(duration))
+
+    // Add the role to the user
+    user.roles.add(role)
+    await prisma.tempRole.create({
+      data: {
+        guildId: interaction.guild.id,
+        userId: user.id,
+        roleId: role.id,
+        expiresAt: expriesAt,
+      },
+    })
+
+    // Reply to the interaction
+    await interaction.editReply({
+      content: `User: ${user} got role: ${role} for duration: ${duration} (expires at: ${time(
+        expriesAt,
+      )})`,
+    })
+  },
+})
