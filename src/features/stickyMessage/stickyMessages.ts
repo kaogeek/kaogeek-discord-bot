c7bb8a21581a261243bca681d791d11d2c00f75f
Meehoi <beam_baimon@hotmail.com>
2023-05-26 21:44:21 +0700

    Make sticky message silent (#153)
    * Make sticky message slient
    
    * Update unit test

--- src/features/stickyMessage/stickyMessages.ts
+++ src/features/stickyMessage/stickyMessages.ts
@@ -1,4 +1,4 @@
-import { DiscordAPIError, Message } from 'discord.js'
+import { DiscordAPIError, Message, MessageFlags } from 'discord.js'
 
 import { StickyMessage } from '@prisma/client'
 
@@ -50,6 +50,7 @@ export async function pushMessageToBottom(
 
     const newMessage = await message.channel.send({
       content: stickyMessage.message,
+      flags: MessageFlags.SuppressNotifications,
     })
 
     // update sticky message with new when successfully send new message
