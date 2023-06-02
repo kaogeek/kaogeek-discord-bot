0733dece75dd098fa9b9b678b0d87094ff0463a1
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-02 19:28:31 +0700

    add roles bypass

--- src/features/preventEmojiSpam/isEmojiPreventionEnabled.spec.ts
+++ src/features/preventEmojiSpam/isEmojiPreventionEnabled.spec.ts
@@ -5,28 +5,46 @@ import { RuntimeConfigurationSchema } from '@/utils/RuntimeConfigurationSchema'
 import { isEmojiPreventionEnabled } from './isEmojiPreventionEnabled'
 
 const config: RuntimeConfigurationSchema['preventEmojiSpam'] = {
-  enabled: true,
+  enabled: false,
   enabledChannels: ['1', '4'],
   disabledChannels: ['2', '4'],
+  bypassRoles: [],
+}
+
+const member = {
+  roles: {
+    cache: {
+      has: (roleId: string) => roleId === '99',
+    },
+  },
 }
 
 test('enabled', () => {
-  expect(isEmojiPreventionEnabled(config, { id: '3' })).toBe(true)
+  const testConfig = { ...config, enabled: true }
+  expect(isEmojiPreventionEnabled(testConfig, { id: '3' }, member)).toBe(true)
 })
 
 test('disabled', () => {
-  const testConfig = { ...config, enabled: false }
-  expect(isEmojiPreventionEnabled(testConfig, { id: '2' })).toBe(false)
+  const testConfig = { ...config }
+  expect(isEmojiPreventionEnabled(testConfig, { id: '2' }, member)).toBe(false)
 })
 
 test('enabledChannels', () => {
-  expect(isEmojiPreventionEnabled(config, { id: '1' })).toBe(true)
+  const testConfig = { ...config }
+  expect(isEmojiPreventionEnabled(testConfig, { id: '1' }, member)).toBe(true)
 })
 
 test('disabledChannels', () => {
-  expect(isEmojiPreventionEnabled(config, { id: '2' })).toBe(false)
+  const testConfig = { ...config, enabled: true }
+  expect(isEmojiPreventionEnabled(testConfig, { id: '2' }, member)).toBe(false)
 })
 
 test('disabledChannels wins over enabledChannels', () => {
-  expect(isEmojiPreventionEnabled(config, { id: '4' })).toBe(false)
+  const testConfig = { ...config, enabled: false }
+  expect(isEmojiPreventionEnabled(testConfig, { id: '4' }, member)).toBe(false)
+})
+
+test('role bypass', () => {
+  const testConfig = { ...config, enabled: true, bypassRoles: ['99'] }
+  expect(isEmojiPreventionEnabled(testConfig, { id: '3' }, member)).toBe(false)
 })
--- src/features/preventEmojiSpam/isEmojiPreventionEnabled.ts
+++ src/features/preventEmojiSpam/isEmojiPreventionEnabled.ts
@@ -5,6 +5,7 @@ import { RuntimeConfigurationSchema } from '@/utils/RuntimeConfigurationSchema'
 export function isEmojiPreventionEnabled(
   config: RuntimeConfigurationSchema['preventEmojiSpam'],
   channel: Pick<Channel, 'id'>,
+  member: { roles: { cache: { has: (roleId: string) => boolean } } },
 ) {
   if (config.disabledChannels.includes(channel.id)) {
     return false
@@ -12,6 +13,10 @@ export function isEmojiPreventionEnabled(
 
   if (config.enabledChannels.includes(channel.id)) {
     return true
+  }
+
+  if (config.bypassRoles.some((roleId) => member.roles.cache.has(roleId))) {
+    return false
   }
 
   return config.enabled
