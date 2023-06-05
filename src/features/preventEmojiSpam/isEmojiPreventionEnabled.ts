078041531e34c2851e4e984b65b6c7fef437e115
LagSeeN <lagseeing_0001@outlook.com>
2023-06-05 20:42:36 +0700

    Reorder code

--- src/features/preventEmojiSpam/isEmojiPreventionEnabled.ts
+++ src/features/preventEmojiSpam/isEmojiPreventionEnabled.ts
@@ -7,16 +7,16 @@ export function isEmojiPreventionEnabled(
   channel: Pick<Channel, 'id'>,
   member: { roles: { cache: { has: (roleId: string) => boolean } } },
 ) {
+  if (config.bypassRoles.some((roleId) => member.roles.cache.has(roleId))) {
+    return false
+  }
+
   if (config.disabledChannels.includes(channel.id)) {
     return false
   }
 
   if (config.enabledChannels.includes(channel.id)) {
     return true
-  }
-
-  if (config.bypassRoles.some((roleId) => member.roles.cache.has(roleId))) {
-    return false
   }
 
   return config.enabled
