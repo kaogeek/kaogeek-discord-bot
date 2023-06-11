47c87c5595b3613377fbb6989c915cbeb2242792
Theeraphat J <96657413+3raphat@users.noreply.github.com>
2023-06-11 15:58:55 +0700

    update temp-role command (#184)
    * rename
    
    * add subcommand remove
    
    * update temporaryRoleCommand.ts
    
    * update temporaryRoleCommand.ts
    
    * fix
    
    * check if the role is not a temporary role
    
    * fix

--- src/features/tempRole/temporaryRoleCommand.ts
+++ src/features/tempRole/temporaryRoleCommand.ts
@@ -1,6 +1,7 @@
 import {
   ApplicationCommandOptionType,
   ApplicationCommandType,
+  CommandInteraction,
   GuildMember,
   PermissionsBitField,
   Role,
@@ -20,22 +21,48 @@ export const temporaryRoleCommand = defineCommand({
     type: ApplicationCommandType.ChatInput,
     options: [
       {
-        name: 'user',
-        description: 'User to set the temporary role for',
-        type: ApplicationCommandOptionType.User,
-        required: true,
+        name: 'add',
+        description: 'Add a temporary role to a user',
+        type: ApplicationCommandOptionType.Subcommand,
+        options: [
+          {
+            name: 'user',
+            description: 'User to set the temporary role for',
+            type: ApplicationCommandOptionType.User,
+            required: true,
+          },
+          {
+            name: 'role',
+            description: 'Role to set',
+            type: ApplicationCommandOptionType.Role,
+            required: true,
+          },
+          {
+            name: 'duration',
+            description: 'Duration of the temporary role e.g. 1d',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+          },
+        ],
       },
       {
-        name: 'role',
-        description: 'Role to set',
-        type: ApplicationCommandOptionType.Role,
-        required: true,
-      },
-      {
-        name: 'duration',
-        description: 'Duration of the temporary role e.g. 1d',
-        type: ApplicationCommandOptionType.String,
-        required: true,
+        name: 'remove',
+        description: 'Remove a temporary role from a user',
+        type: ApplicationCommandOptionType.Subcommand,
+        options: [
+          {
+            name: 'user',
+            description: 'User to remove the temporary role from',
+            type: ApplicationCommandOptionType.User,
+            required: true,
+          },
+          {
+            name: 'role',
+            description: 'Role to remove',
+            type: ApplicationCommandOptionType.Role,
+            required: true,
+          },
+        ],
       },
     ],
   },
@@ -52,62 +79,139 @@ export const temporaryRoleCommand = defineCommand({
     // Get duration from the interaction
     const duration = interaction.options.getString('duration') as string
 
-    // Parse the duration
-    const durationMs = parse(duration)
-
-    // Check if the duration is invalid
-    if (!durationMs) {
-      await interaction.editReply({
-        content: `Invalid duration: ${duration}`,
-      })
-      return
-    }
-
-    // Check if the user already has the role
-    if (user.roles.cache.has(role.id)) {
-      await interaction.editReply({
-        content: `User: ${user} already has role: ${role}`,
-      })
-      return
-    }
-
-    const expiresAt = new Date(Date.now() + durationMs)
-
-    // Add the role to the user
-    try {
-      await user.roles.add(role)
-
-      // Add the temporary role to the database
-      try {
-        await prisma.tempRole.create({
-          data: {
-            guildId: interaction.guild.id,
-            userId: user.id,
-            roleId: role.id,
-            expiresAt: expiresAt,
-          },
-        })
-      } catch (error) {
-        console.error(
-          `Failed to add temp role to database`,
-          (error as Error).message,
-        )
-      }
-
-      // Reply to the interaction
-      await interaction.editReply({
-        content: `User: ${user} got role: ${role} for duration: ${duration} (expires at: ${time(
-          expiresAt,
-        )})`,
-      })
-    } catch (error) {
-      console.error(
-        `Failed to add role "${role.name}" to "${user.user.tag}"`,
-        (error as Error).message,
-      )
-      await interaction.editReply({
-        content: `Failed to add role: ${role} to user: ${user}`,
-      })
+    if (interaction.options.getSubcommand() === 'add') {
+      await temporaryRoleAdd(interaction, user, role, duration)
+    } else if (interaction.options.getSubcommand() === 'remove') {
+      await temporaryRoleRemove(interaction, user, role)
     }
   },
 })
+
+async function temporaryRoleAdd(
+  interaction: CommandInteraction,
+  user: GuildMember,
+  role: Role,
+  duration: string,
+) {
+  // Parse the duration
+  const durationMs = parse(duration)
+
+  // Check if the duration is invalid
+  if (!durationMs) {
+    await interaction.editReply({
+      content: `Invalid duration: ${duration}`,
+    })
+    return
+  }
+
+  // Check if the user already has the role
+  if (user.roles.cache.has(role.id)) {
+    await interaction.editReply({
+      content: `User: ${user} already has role: ${role}`,
+    })
+    return
+  }
+
+  const expiresAt = new Date(Date.now() + durationMs)
+
+  try {
+    // Add the role to the user
+    await user.roles.add(role)
+
+    // Add the temporary role to the database
+    try {
+      await prisma.tempRole.create({
+        data: {
+          guildId: interaction.guildId as string,
+          userId: user.id,
+          roleId: role.id,
+          expiresAt: expiresAt,
+        },
+      })
+    } catch (error) {
+      console.error(
+        `Failed to add temp role to database`,
+        (error as Error).message,
+      )
+    }
+
+    // Reply to the interaction
+    await interaction.editReply({
+      content: `User: ${user} got role: ${role} for duration: ${duration} (expires at: ${time(
+        expiresAt,
+      )})`,
+    })
+  } catch (error) {
+    console.error(
+      `Failed to add role "${role.name}" to "${user.user.tag}"`,
+      (error as Error).message,
+    )
+    await interaction.editReply({
+      content: `Failed to add role: ${role} to user: ${user}`,
+    })
+  }
+}
+
+async function temporaryRoleRemove(
+  interaction: CommandInteraction,
+  user: GuildMember,
+  role: Role,
+) {
+  // Check if the role is not a temporary role
+  if (
+    user.roles.cache.has(role.id) &&
+    !(await prisma.tempRole.findFirst({
+      where: {
+        guildId: interaction.guildId as string,
+        userId: user.id,
+        roleId: role.id,
+      },
+    }))
+  ) {
+    await interaction.editReply({
+      content: `Cannot remove role: ${role} from user: ${user} because it's not a temporary role`,
+    })
+    return
+  }
+
+  // Check if the user not has the role
+  if (!user.roles.cache.has(role.id)) {
+    await interaction.editReply({
+      content: `User: ${user} does not have role: ${role}`,
+    })
+    return
+  }
+
+  try {
+    // Remove the role from the user
+    await user.roles.remove(role)
+
+    // Remove the temporary role from the database
+    try {
+      await prisma.tempRole.deleteMany({
+        where: {
+          guildId: interaction.guildId as string,
+          userId: user.id,
+          roleId: role.id,
+        },
+      })
+    } catch (error) {
+      console.error(
+        `Failed to remove temp role from database`,
+        (error as Error).message,
+      )
+    }
+
+    // Reply to the interaction
+    await interaction.editReply({
+      content: `User: ${user} lost role: ${role}`,
+    })
+  } catch {
+    console.error(
+      `Failed to remove role "${role.name}" from "${user.user.tag}"`,
+    )
+    await interaction.editReply({
+      content: `Failed to remove role: ${role} from user: ${user}`,
+    })
+  }
+}
